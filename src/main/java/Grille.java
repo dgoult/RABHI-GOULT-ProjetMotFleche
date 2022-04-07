2d8bf0dc3c4f243f18a231b9a8c3b2240ab0a7e9
dgoult <dylan.goult@limayrac.fr>
2022-04-07 16:00:17 +0200

    Création de la classe grille

---
+++ src/main/java/Grille.java
@@ -0,0 +1,25 @@
+public class Grille {
+    private int hauteur;
+    private int largeur;
+
+    public Grille(int hauteur, int largeur) {
+        this.hauteur = hauteur;
+        this.largeur = largeur;
+    }
+
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    public void setHauteur(int hauteur) {
+        this.hauteur = hauteur;
+    }
+
+    public int getLargeur() {
+        return largeur;
+    }
+
+    public void setLargeur(int largeur) {
+        this.largeur = largeur;
+    }
+}
