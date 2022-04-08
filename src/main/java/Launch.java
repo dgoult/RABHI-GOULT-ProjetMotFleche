25ee7d34045089933d8ff29a232d167388fba8c5
dgoult <dylan.goult@limayrac.fr>
2022-04-08 16:30:33 +0200

    Ajout méthode ajoutMot et affichage console

--- src/main/java/Launch.java
+++ src/main/java/Launch.java
@@ -3,6 +3,10 @@ import model.Grille;
 public class Launch {
     public static void main(String args[]) {
         Grille grille = new Grille(9, 9);
+//        grille.Afficher();
+//        grille.ajouterMotHorizontal("totem", 0,2);
+        grille.Afficher();
+        grille.ajouterMotVertical("vertica", 0, 0);
         grille.Afficher();
     }
 }
--- src/main/java/model/CaseLettre.java
+++ src/main/java/model/CaseLettre.java
@@ -1,5 +1,25 @@
 package model;
 
+import enumeration.Dir;
+
 public abstract class CaseLettre implements Case {
-//TODO: faire la classe case lettre
+    Dir direction;
+    Coordonnee coordonneeLettre;
+    String definition;
+    int nbMot = 1;
+
+    public CaseLettre(Coordonnee coordonneeLettre, int nbMot) {
+        this.coordonneeLettre = coordonneeLettre;
+        this.nbMot = nbMot;
+    }
+
+    @Override
+    public Coordonnee getCoordonnee() {
+        return this.coordonneeLettre;
+    }
+
+    @Override
+    public Coordonnee setCoordonnee(Coordonnee nouvelleCoordonnee) {
+        return this.coordonneeLettre = nouvelleCoordonnee;
+    }
 }
